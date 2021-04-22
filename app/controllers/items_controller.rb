4832fcc0ee154128d76b3440f006e3322c0b6ee5
lavie666 <lavie.progra327@outlook.jp>
2021-04-22 15:00:06 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, except: [:index, :show,]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, except: [:index, :new, :create, :show, :destroy]
+  before_action :move_to_index, except: [:index, :new, :create, :show,]
 
   def index
     @items = Item.order("created_at DESC")
@@ -35,9 +35,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
-      redirect_to root_path
-    end 
+    @item.destroy
   end
 
   private
